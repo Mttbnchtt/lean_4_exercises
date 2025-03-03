4ffdd5eb680c62048cb129801c4ea11c94f2f282
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-03-03 14:27:12 -0500

    Auto-commit at 2025-03-03 14:27:12; modified: exercises_functional_programming_in_lean/1.1_evaluating_expressions.lean

--- exercises_functional_programming_in_lean/1.1_evaluating_expressions.lean
+++ exercises_functional_programming_in_lean/1.1_evaluating_expressions.lean
@@ -2,3 +2,8 @@
 #eval 1 + 2 * 5
 #eval String.append "Hello, " "Lean"
 #eval String.append "great " (String.append "oak " "tree ")
+#eval 1 + 3 + 4
+#eval 1 + (3 + 4)
+#eval String.append "it is " (if 1 > 2 then "yes" else "no")
+#eval String.append "it is " (if false then "yes" else "no")
+#eval String.append "it is " (if true then "yes" else "no")
