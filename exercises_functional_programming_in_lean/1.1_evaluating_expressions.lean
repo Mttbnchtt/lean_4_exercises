60918939fd304ed7e27f14ddfd43327eb54f4f5e
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-03-04 07:22:20 -0500

    Auto-commit at 2025-03-04 07:22:20; modified: exercises_functional_programming_in_lean/1.1_evaluating_expressions.lean

--- exercises_functional_programming_in_lean/1.1_evaluating_expressions.lean
+++ exercises_functional_programming_in_lean/1.1_evaluating_expressions.lean
@@ -7,3 +7,10 @@
 #eval String.append "it is " (if 1 > 2 then "yes" else "no")
 #eval String.append "it is " (if false then "yes" else "no")
 #eval String.append "it is " (if true then "yes" else "no")
+
+#eval 42 + 19
+#eval String.append "A" (String.append "B" "C")
+#eval String.append (String.append "A" "B") "C"
+#eval String.append "A" (String.append "B" "C") == String.append (String.append "A" "B") "C"
+#eval if 3 == 3 then 5 else 7
+#eval if 3 == 4 then "equal" else "not equal"
