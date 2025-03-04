3b329b47ed3199fc14dc71a990f4ecae8be139c5
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-03-04 13:13:36 -0500

    Auto-commit at 2025-03-04 13:13:36; modified: exercises_functional_programming_in_lean/1.3_functions_and_definitions.lean

--- exercises_functional_programming_in_lean/1.3_functions_and_definitions.lean
+++ exercises_functional_programming_in_lean/1.3_functions_and_definitions.lean
@@ -5,3 +5,26 @@ def lean : String := "Lean"
 def append_strings (s1 s2 s3 : String) : String := String.append s1 (String.append s2 s3)
 #eval append_strings "A" "B" "C"
 #eval append_strings hello " " lean
+#check append_strings
+
+def successor (n : Nat) : Nat := n + 1
+#eval successor 3
+#check successor
+#check successor 3
+
+def maximum (n : Nat) (m : Nat) : Nat :=
+  if n > m then
+    n
+  else m
+#eval maximum 3 4
+#eval maximum (4*3) (2*7)
+#check maximum
+#check (maximum)
+
+def joinStringWith (str1 str2 str3 : String) : String := String.append str2 (String.append str1 str3)
+#eval joinStringWith "A" "B" "C"
+#check (joinStringWith)
+
+def volume (h w d : Nat) : Nat := h * w * d
+#eval volume 3 4 5
+#check (volume)
