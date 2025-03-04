ae23aa956de8605b024079ce2c4fad0fd3ca02a1
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-03-04 11:50:29 -0500

    Auto-commit at 2025-03-04 11:50:29; modified: exercises_functional_programming_in_lean/1.3_functions_and_definitions.lean

--- exercises_functional_programming_in_lean/1.3_functions_and_definitions.lean
+++ exercises_functional_programming_in_lean/1.3_functions_and_definitions.lean
@@ -1,1 +1,7 @@
 def hello := "Hello"
+def lean : String := "Lean"
+#eval String.append hello (String.append " " lean)
+
+def append_strings (s1 s2 s3 : String) : String := String.append s1 (String.append s2 s3)
+#eval append_strings "A" "B" "C"
+#eval append_strings hello " " lean
