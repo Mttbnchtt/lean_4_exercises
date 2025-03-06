6a17924a6f429c428ba57b580070db2dd6c412d2
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-03-06 11:04:16 -0500

    Auto-commit at 2025-03-06 11:04:16; modified: exercises_functional_programming_in_lean/1.4_structures.lean

--- exercises_functional_programming_in_lean/1.4_structures.lean
+++ exercises_functional_programming_in_lean/1.4_structures.lean
@@ -38,6 +38,7 @@ def addPoints (p1 p2 : Point) : Point := { x := p1.x + p2.x, y := p1.y + p2.y}
 
 def distance (p1 p2 : Point) : Float :=
   Float.sqrt ((p1.x - p2.x)^2 + (p1.y - p2.y)^2)
+
 #eval distance p origin
 #check (distance)
 #eval distance { x := 1.0, y := 2.0 } { x := 5.0, y := -1.0}
@@ -58,4 +59,53 @@ def zeroX' (p : Point) : Point := { p with x := 0.0}
 #eval "A".length + "B".length
 #eval String.append "A" "B"
 
-def Point.modifyBoth
+def Point.modifyBoth (f : Float -> Float) (p : Point) : Point :=
+  { x := f p.x, y := f p.y }
+#eval Point.modifyBoth Float.floor origin
+#eval Point.modifyBoth Float.floor p
+
+def scale (p : Point) : Point :=
+  Point.modifyBoth (λ x => x * 2) p
+
+#eval scale origin
+#eval scale p
+#eval p.modifyBoth Float.floor
+
+def Point.scale (p : Point) : Point :=
+  Point.modifyBoth (λ x => x * 2) p
+#eval p.scale
+#eval Point.scale p
+
+
+-- Exercise 1.4.1
+structure RectangularPrism where
+  width : Float
+  height : Float
+  depth : Float
+deriving Repr
+
+-- Exercise 1.4.2
+def volume (p : RectangularPrism) : Float :=
+  p.width * p.height * p.depth
+
+-- Exercise 1.4.3
+structure Segment where
+  start_point : Point
+  end_point : Point
+deriving Repr
+
+def length (s : Segment) : Float :=
+  distance s.start_point s.end_point
+
+def s : Segment := {
+    start_point := { x := 1.0, y := 2.0 },
+    end_point := { x := 5.0, y := -1.0 }
+  }
+#eval length s
+
+-- Exercise 1.4.4
+-- RectangularPrism, RectangularPrism.mk, RectangularPrism.widt, RectangularPrism.height, RectangularPrism.depth
+
+-- Exercise 1.4.5
+-- Hamster, Hamster.mk, Hamster.name, Hamster.fluffy
+-- Book, Book.mk, Book.title, Book.author, Book.price
