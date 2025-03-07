4ca75cf8eb73fc3e6a1e1c287e9d95723748477e
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-03-07 13:48:50 -0500

    Auto-commit at 2025-03-07 13:48:50; modified: exercises_functional_programming_in_lean/1.5_datatypes_patterns_and_recursion.lean

--- exercises_functional_programming_in_lean/1.5_datatypes_patterns_and_recursion.lean
+++ exercises_functional_programming_in_lean/1.5_datatypes_patterns_and_recursion.lean
@@ -1,3 +1,52 @@
-inductive Bool where
-  | true : Bool
-  | false : Bool
+-- inductive Bool where
+--   | true : Bool
+--   | false : Bool
+
+-- inductive Nat where
+--   | zero : Nat
+--   | succ : Nat → Nat
+
+-- inductive Nat where
+--   | zero : Nat
+--   | succ (n : Nat) : Nat
+
+def isZero : Nat → Bool
+  | Nat.zero => Bool.true
+  | _ => Bool.false
+
+def pred (n : Nat) : Nat :=
+  match n with
+  | Nat.zero => Nat.zero
+  | Nat.succ k => k
+
+def even (n : Nat) : Bool :=
+  match n with
+  | Nat.zero              => Bool.true
+  | Nat.succ (Nat.succ k) => even k
+  | _                     => Bool.false
+
+#eval even 0
+#eval even 3
+
+def even_v2 (n : Nat) : Bool :=
+  match n with
+  | Nat.zero => Bool.true
+  | Nat.succ k => not (even_v2 k)
+
+#eval even_v2 0
+#eval even_v2 3
+
+def even_v3 (n : Nat) : Bool :=
+  match n with
+  | Nat.zero => Bool.true
+  | Nat.succ (Nat.succ k) =>
+      if even_v3 k then Bool.true else Bool.false
+  | _ => Bool.false
+
+#eval even_v3 0
+#eval even_v3 3
+
+def p (n m : Nat) : Nat :=
+  match m with
+  | Nat.zero => n
+  | Nat.succ k => Nat.succ p n m
