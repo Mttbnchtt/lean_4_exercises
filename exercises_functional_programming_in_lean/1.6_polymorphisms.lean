58a231a992ad9dcf78006257609e28640fb9edd3
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-03-10 07:07:23 -0400

    Auto-commit at 2025-03-10 07:07:22; modified: exercises_functional_programming_in_lean/1.6_polymorphisms.lean

--- exercises_functional_programming_in_lean/1.6_polymorphisms.lean
+++ exercises_functional_programming_in_lean/1.6_polymorphisms.lean
@@ -35,3 +35,7 @@ def posOrNegThree (s : Sign) : match s with | Sign.pos => Nat | Sign.neg => Int 
 #eval posOrNegThree Sign.pos
 #eval posOrNegThree Sign.neg
 #eval posOrNegThree Sign.zero
+
+def d ( n : Nat) : List Nat :=
+  List.filter (fun x => (¬ (x = 0)) ∧ (n % x = 0)) (List.range (n+1))
+#eval d 10
