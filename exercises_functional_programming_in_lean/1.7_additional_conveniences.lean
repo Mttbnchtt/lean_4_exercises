611d3e7f022a4eb18b41204201ab8fa19c68da8e
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-03-19 06:31:57 -0400

    Auto-commit at 2025-03-19 06:31:57; modified: exercises_functional_programming_in_lean/1.7_additional_conveniences.lean

--- exercises_functional_programming_in_lean/1.7_additional_conveniences.lean
+++ exercises_functional_programming_in_lean/1.7_additional_conveniences.lean
@@ -1,4 +1,6 @@
 def drop : Nat → List α → List α
-| 0, xs => xs
-| _, [] => []
-| n+1, x :: xs => drop n xs
+  | 0, xs => xs
+  | _, [] => []
+  | n+1, x :: xs => drop n xs
+
+#eval drop 2 [1, 2, 3, 4, 5]  -- [3, 4, 5]s
