57acee2c4c4269ecfbc1a24c733e8cb760ca4ed0
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-04-14 11:19:45 -0400

    Auto-commit at 2025-04-14 11:19:45; modified: exercises_functional_programming_in_lean/3_interlude_propositions_proof_and_indexing.lean

--- exercises_functional_programming_in_lean/3_interlude_propositions_proof_and_indexing.lean
+++ exercises_functional_programming_in_lean/3_interlude_propositions_proof_and_indexing.lean
@@ -12,3 +12,15 @@ theorem onePlusOneIsTwo : 1 + 1 = 2 := by
 -- decide: This tactic attempts to automatically prove goals that are decidable propositions. It’s especially useful for goals that can be resolved through computation, such as basic arithmetic equalities or inequalities.
 
 #check onePlusOneIsTwo  -- Outputs: true
+
+theorem addAndAppend : 1 + 1 = 2 ∧ "Str".append "ing" = "String" := by
+  constructor
+  decide
+  decide
+  -- The constructor tactic is used to prove conjunctions (∧) and disjunctions (∨).
+  -- In this case, it splits the goal into two subgoals: one for each part of the conjunction.
+  -- The decide tactic is then used to prove each subgoal.
+  -- The first subgoal is to prove that 1 + 1 = 2, and the second subgoal is to prove that "Str".append "ing" = "String".
+  -- The decide tactic can handle both of these goals, as they are decidable propositions.
+
+#check addAndAppend  -- Outputs: true
