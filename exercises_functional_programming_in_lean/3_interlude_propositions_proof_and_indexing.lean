dd6c8284ae42a2fc356eabdf9b74912360db74b5
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-04-14 11:11:59 -0400

    Auto-commit at 2025-04-14 11:11:59; modified: exercises_functional_programming_in_lean/3_interlude_propositions_proof_and_indexing.lean

--- exercises_functional_programming_in_lean/3_interlude_propositions_proof_and_indexing.lean
+++ exercises_functional_programming_in_lean/3_interlude_propositions_proof_and_indexing.lean
@@ -4,3 +4,13 @@ def woodlandCritters : List String :=
 def hedgehog : String := (woodlandCritters[0]?).getD "unknown"
 
 #eval hedgehog  -- Outputs: "hedgehog"
+
+theorem onePlusOneIsTwo : 1 + 1 = 2 := by
+  simp
+
+  -- The proof is trivial, but we can use the tactic `decide` to automatically prove it.
+  -- decide
+  -- The `decide` tactic will automatically prove the goal if it can be decided by a simple computation.
+  -- In this case, it can be decided because `1 + 1` is equal to `2`.
+
+eval onePlusOneIsTwo  -- Outputs: true
