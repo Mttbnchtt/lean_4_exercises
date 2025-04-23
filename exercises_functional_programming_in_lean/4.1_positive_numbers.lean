28308932675965460bdb4399d4f4b29a510969f3
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-04-23 07:35:49 -0400

    Auto-commit at 2025-04-23 07:35:49; modified: exercises_functional_programming_in_lean/4.1_positive_numbers.lean

--- exercises_functional_programming_in_lean/4.1_positive_numbers.lean
+++ exercises_functional_programming_in_lean/4.1_positive_numbers.lean
@@ -1,3 +1,8 @@
 structure PositiveNumbers where
   successor ::           -- the named constructor
   predecessor : Nat      -- stores n  so that the actual value is n+1
+
+def PositiveNumbers.add (x y : PositiveNumbers) : PositiveNumbers :=
+  successor (x.predecessor + y.predecessor + 1)
+
+#eval PositiveNumbers.add (PositiveNumbers.successor 1) (PositiveNumbers.successor 2)
