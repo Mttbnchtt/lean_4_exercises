1baa3b06cbee1b885ec577ee65e16e8b7af3d7b2
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-04-25 11:05:26 -0400

    Auto-commit at 2025-04-25 11:05:26; modified: exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_by_calculation.lean

--- exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_by_calculation.lean
+++ exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_by_calculation.lean
@@ -20,9 +20,22 @@ import Mathlib
 theorem theorem_1_1_1
   (a b : ℚ)
   (hyp_1 : a -b = 4)
-  (hyp_2 : ab = 1)
+  (hyp_2 : a*b = 1)
   : (a + b)^2 = 20 := by
   calc
     (a + b)^2 = a^2 + 2*a*b + b^2 := by exact add_pow_two a b
     _         = a^2 +2*a*b + b^2 +2*a*b - 2*a*b := by exact Eq.symm (add_sub_cancel_right (a ^ 2 + 2 * a * b + b ^ 2) (2 * a * b))
-    _         = a^2 - 2*a*b + b^2 +4*a*b := by refine Eq.symm (Rat.ext ?_ ?_)
+    _         = a^2 +2*a*b + b^2 - 2*a*b + 2*a*b := by simp
+    _         = a^2 +2*a*b + (b^2 - 2*a*b) + 2*a*b := by ring
+    _         = a^2 +2*a*b + (- 2*a*b + b^2) + 2*a*b := by ring
+    _         = a^2 + (- 2*a*b + b^2) + 2*a*b + 2*a*b := by ring
+    _         = a^2 + (- 2*a*b + b^2) + (2*a*b + 2*a*b) := by ring
+    _         = a^2 + (- 2*a*b + b^2) + 4*a*b := by ring
+    _         = (a^2 - 2*a*b + b^2) + 4*a*b := by ring
+    _         = (a - b)^2 + 4*a*b := by ring
+    _         = 4^2 + 4*a*b := by rw [hyp_1]
+    _         = 16 + 4*a*b := by exact rfl
+    _         = 16 + 4*(a*b) := by ring
+    _         = 16 + 4*1 := by rw [hyp_2]
+    _         = 16 + 4 := by simp
+    _         = 20 := by norm_num
