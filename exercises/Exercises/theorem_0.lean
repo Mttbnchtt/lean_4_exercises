b91244814368dc32d7b718f5d976494a38521146
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-04-25 09:39:31 -0400

    Auto-commit at 2025-04-25 09:39:31; modified: exercises/Exercises/theorem_0.lean

--- exercises/Exercises/theorem_0.lean
+++ exercises/Exercises/theorem_0.lean
@@ -1,8 +1,7 @@
 import Mathlib
 
 theorem test_theorem (α β : ℝ) (hαβ : α - β = 4) : α = β + 4 := by
-  -- exact Eq.symm (add_eq_of_eq_sub' (Eq.symm hαβ))
   calc
     α = α - β + β := by exact Eq.symm (sub_add_cancel α β)
-    _ = 4 + β := by rw hαβ
-    _ = β + 4 := by sorry
+    _ = 4 + β := by rw [hαβ]
+    _ = β + 4 := by rw [add_comm]
