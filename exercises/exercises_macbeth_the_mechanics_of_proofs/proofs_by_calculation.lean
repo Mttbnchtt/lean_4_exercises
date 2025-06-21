cb897a1044b3dd916adc8f494d9ba27706e5ca95
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-06-21 07:34:55 -0400

    Auto-commit at 2025-06-21 07:34:54; modified: exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_by_calculation.lean, exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_I.lean

--- exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_by_calculation.lean
+++ exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_by_calculation.lean
@@ -790,18 +790,7 @@ theorem theorem_1_4_11_7
     _         = (a - b)^2 + 2*a*b := by ring
     _         ≥ 2*a*b := by nlinarith
 
--- -----------------------------
-theorem theorem_2_1
-  (a b : ℝ)
-  (h1 : a - 5*b = 4)
-  (h2 : b + 2 = 3)
-  : a = 9 := by
-    have h3 : b = 1 := by linarith [h2]
-    calc
-       a = a - 5*b + 5*b := by ring
-       _ = 4 + 5*b := by rw [h1]
-       _ = 4 + 5*1 := by rw [h3]
-       _ = 9 := by linarith
+
 
 -- •	Option + Shift + DownArrow (⌥⇧↓) to copy the line below
 -- •	Option + Shift + UpArrow (⌥⇧↑) to copy the line above  ￼
---
+++ exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_I.lean
@@ -0,0 +1,13 @@
+import Mathlib
+
+theorem theorem_2_1
+  (a b : ℝ)
+  (h1 : a - 5*b = 4)
+  (h2 : b + 2 = 3)
+  : a = 9 := by
+    have h3 : b = 1 := by linarith [h2]
+    calc
+       a = a - 5*b + 5*b := by ring
+       _ = 4 + 5*b := by rw [h1]
+       _ = 4 + 5*1 := by rw [h3]
+       _ = 9 := by linarith
