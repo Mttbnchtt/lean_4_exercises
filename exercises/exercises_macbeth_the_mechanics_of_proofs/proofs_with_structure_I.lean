aae3be480a10e4f9451a78964b749731727c5488
Matteo Bianchetti <mttbnchtt@gmail.com>
2025-06-23 07:19:28 -0700

    Auto-commit at 2025-06-23 07:19:28; modified: exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_I.lean

--- exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_I.lean
+++ exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_I.lean
@@ -11,3 +11,19 @@ theorem theorem_2_1
        _ = 4 + 5*b := by rw [h1]
        _ = 4 + 5*1 := by rw [h3]
        _ = 9 := by linarith
+
+
+
+theorem theorem_2_1_2
+  (m n :ℤ)
+  (h1 : m + 3 ≤ 2*n - 1)
+  (h2 : n ≤ 5)
+  : m ≤ 6 := by
+  have h3: m ≤ 2*n - 4 := by
+    calc m = m + 3 - 3 := by ring
+         _ ≤ 2*n - 1 - 3 := by rel [h1]
+         _ ≤ 2*n - 4 := by linarith
+  calc
+    m ≤ 2*n - 4 := by rel [h3]
+    _ ≤ 2*5 - 4 := by rel [h2]
+    _ = 6 := by linarith
