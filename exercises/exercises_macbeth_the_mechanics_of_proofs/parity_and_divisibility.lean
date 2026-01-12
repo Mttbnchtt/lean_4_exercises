b096ba3d294dd4454781825448105fc1fe3d57f9
Matteo Bianchetti <mttbnchtt@gmail.com>
2026-01-12 12:41:37 -0500

    Auto-commit at 2026-01-12 12:41:37; modified: exercises/exercises_macbeth_the_mechanics_of_proofs/parity_and_divisibility.lean

--- exercises/exercises_macbeth_the_mechanics_of_proofs/parity_and_divisibility.lean
+++ exercises/exercises_macbeth_the_mechanics_of_proofs/parity_and_divisibility.lean
@@ -38,3 +38,25 @@ example
   calc
     3 * n + 2 = 3 * (2 * k + 1) + 2 := by rw [hk]
     _ = 2 * (3 * k + 2) + 1 := by ring
+
+
+example
+  {n : ℤ}
+  (hn : Odd n)
+  : Odd (7 * n - 4) := by
+  dsimp [Odd] at *
+  rcases hn with ⟨ k, hk ⟩
+  use (7*k + 1)
+  calc
+    7*n - 4 = 7*(2*k + 1) - 4  := by rw [hk]
+    _       = 14*k + 3        := by linarith
+    _       = 2*(7*k + 1) + 1 := by linarith
+
+example
+  {n : ℤ}
+  (hn : Odd n)
+  : Odd (7 * n - 4) := by
+  dsimp [Odd] at *
+  rcases hn with ⟨ k, hk ⟩
+  use (7*k + 1)
+  linarith
