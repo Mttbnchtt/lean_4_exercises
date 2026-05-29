c6b1ec299c3b43eeddc48a944865c95766d3d443
Matteo Bianchetti <mttbnchtt@gmail.com>
2026-05-29 15:50:55 -0400

    Auto-commit at 2026-05-29 15:50:55; modified: exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_II.lean

--- exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_II.lean
+++ exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_II.lean
@@ -264,3 +264,24 @@ example
       _ = 4 + 5*b       := by rw [h1]
       _ = 4 + 5*1       := by rw [g1]
       _ = 9             := by ring
+
+
+example
+  {a b : ℝ}
+  (h1 : a - 5 * b = 4)
+  (h2 : b + 2 = 3)
+  : a = 9 ∧ b = 1 := by
+  have g1 : b = 1 := by
+    calc
+      b = b + 2 - 2 := by ring
+      _ = 3 - 2     := by rw [h2]
+      _ = 1         := by ring
+  constructor
+  case right =>
+    exact g1
+  case left =>
+    calc
+      a = a - 5*b + 5*b := by ring
+      _ = 4 + 5*b       := by rw [h1]
+      _ = 4 + 5*1       := by rw [g1]
+      _ = 9             := by ring
