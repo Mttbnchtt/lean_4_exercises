09a06af46e558f7c604ab91cc7d857cce7ea3d95
Matteo Bianchetti <mttbnchtt@gmail.com>
2026-05-29 15:50:41 -0400

    Auto-commit at 2026-05-29 15:50:41; modified: exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_II.lean

--- exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_II.lean
+++ exercises/exercises_macbeth_the_mechanics_of_proofs/proofs_with_structure_II.lean
@@ -246,3 +246,21 @@ example
   calc
     p ≥ -3 := by nlinarith [g3]
     _ ≥ -5 := by nlinarith
+
+
+example
+  {a b : ℝ}
+  (h1 : a - 5 * b = 4)
+  (h2 : b + 2 = 3)
+  : a = 9 ∧ b = 1 := by
+  have g1 : b = 1 := by
+    linarith [h2]
+  constructor
+  case right =>
+    exact g1
+  case left =>
+    calc
+      a = a - 5*b + 5*b := by ring
+      _ = 4 + 5*b       := by rw [h1]
+      _ = 4 + 5*1       := by rw [g1]
+      _ = 9             := by ring
